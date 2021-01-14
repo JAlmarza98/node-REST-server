854c1419703f7119678ed530ba076e44808f23a2
JAlmarza98 <j.almarza98@gmail.com>
2021-01-14 20:09:47 +0100

    Añadida la base de datos en Mongo Atlas

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,28 @@
 
-// =================================
+// ==========================================
 // Configuracion dinamica del puerto
-// =================================
+// ==========================================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ==========================================
+// Configuracion dinamica del entorno
+// ==========================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ==========================================
+// Configuracion dinamica de la Base de Datos
+// ==========================================
+
+let urlDB;
+
+if( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe-node';
+} else {
+    urlDB = 'mongodb+srv://JAlmarza98:cafe1234@cluster0.jqf60.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use( require('./routes/usuario'));
 
-mongoose.connect('mongodb://localhost:27017/cafe-node',{useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
+mongoose.connect( process.env.URLDB, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, (err, res) => {
 
     if( err ) throw err;
 
